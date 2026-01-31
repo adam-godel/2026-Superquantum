59c4de791c18a5cba3ff400af7a99c2efce9b290
Adam Godel <agodel@bu.edu>
2026-01-31 12:57:15 -0500

    Add unitary 5

--- unitary/unitary5.py
+++ unitary/unitary5.py
@@ -6,19 +6,15 @@ import math
 theta = math.pi / 7
 epsilon = 1e-10
 
-qc_zz = QuantumCircuit(2)
-qc_zz.cx(0, 1)
-qc_zz.append(Rz(2*theta, epsilon).to_gate(), [1])
-qc_zz.cx(0, 1)
-
-qc_xxyy = QuantumCircuit(2)
-qc_xxyy.append(Ry(theta, epsilon).to_gate(), [1])
-qc_xxyy.cx(0, 1)
-qc_xxyy.append(Rz(-theta, epsilon).to_gate(), [0])
-qc_xxyy.append(Ry(-theta, epsilon).to_gate(), [1])
-qc_xxyy.cx(0, 1)
-qc_xxyy.append(Ry(theta, epsilon).to_gate(), [1])
+qc = QuantumCircuit(2)
+qc.h(0); qc.h(1)
+qc.h(1); qc.cx(0, 1); qc.h(1) # cz gate
+qc.cx(0, 1); qc.cx(1, 0); qc.cx(0, 1) # swap gate
+qc.append(Rz(-4*theta, epsilon).to_gate(), [0])
+qc.cx(0, 1); qc.cx(1, 0); qc.cx(0, 1) # swap gate
+qc.h(1); qc.cx(0, 1); qc.h(1) # cz gate
+qc.h(0); qc.h(1)
 
 qasm3_str = dumps3(qc)
-with open("qasm/unitary4.qasm", 'w') as file:
+with open("qasm/unitary5.qasm", 'w') as file:
     file.write(qasm3_str)
